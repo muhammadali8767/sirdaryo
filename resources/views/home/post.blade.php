ddc3abb82356d2f1ad2dfeaa7504d51f3d0a3ead
Muhammadali <muhammadali8767@gmail.com>
2022-12-16 23:07:38 +0500

    asd

--- resources/views/home/post.blade.php
+++ resources/views/home/post.blade.php
@@ -6,18 +6,21 @@
     <div class="container">
         <nav aria-label="breadcrumb" class="breadcrumb-nav">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ route('main') }}" class="breadcrumb-link">@lang('menu.Bosh sahifa')</a></li>
-                <li class="breadcrumb-item"><a href="{{ route('news') }}" class="breadcrumb-link">@lang('menu.Yangiliklar')</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('main') }}" class="breadcrumb-link">@lang('menu.main')</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('news') }}" class="breadcrumb-link">@lang('menu.news')</a></li>
                 <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">{{ $post->title }}</a></li>
             </ol>
         </nav>
         <h2 class="section-title">{{ $post->title }}</h2>
-        <img src="{{ $post->img }}" alt="No image" style="width: 100%">
+        <div class="row">
+            <div class="col-12 offset-0">
+                <img src="{{ $post->img }}" alt="" width="100%">
+            </div>
+        </div>
         <hr>
         {!! $post->text !!}
 
         @include('layouts.components.copy')
-    </div>
     </div>
 </section>
 
