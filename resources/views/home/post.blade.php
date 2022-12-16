a1b11703e7a86e87ca36e5d3cb315a6b5f814a96
Muhammadali <muhammadali8767@gmail.com>
2022-12-16 23:07:38 +0500

    asd

--- resources/views/home/post.blade.php
+++ resources/views/home/post.blade.php
@@ -12,26 +12,15 @@
             </ol>
         </nav>
         <h2 class="section-title">{{ $post->title }}</h2>
-        {!! $post->text !!}
-        <div class="copy-elements">
-            <div class="date-view order-0">
-                <i class="fas fa-calendar"></i>
-                <p>
-                    <span class="hours">{{ $post->created_at->format('H:i') }}</span> / <span class="date">{{ $post->created_at->format('d.m.Y') }}</span>
-                </p>
-            </div>
-            <button class="copy-button order-2 order-sm-1">
-                <i class="fal fa-clone"></i>
-                @lang('front.Nusxa olish')
-            </button>
-            <div class="date-view order-1 order-sm-2">
-                <i class="fas fa-eye"></i>
-                <p>
-                    <span class="view-numbers">{{ $post->seen }}</span>
-                </p>
+        <div class="row">
+            <div class="col-12 offset-0">
+                <img src="{{ $post->img }}" alt="" width="100%">
             </div>
         </div>
-    </div>
+        <hr>
+        {!! $post->text !!}
+
+        @include('layouts.components.copy')
     </div>
 </section>
 
