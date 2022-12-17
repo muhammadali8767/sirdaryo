f098a45ba7eeded1760e97d11f1bf67813f7fa70
Muhammadali <muhammadali8767@gmail.com>
2022-12-17 23:20:04 +0500

    last_changes

--- resources/views/admin/openData/edit.blade.php
+++ resources/views/admin/openData/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin_layout')
 
-@section('title', 'Редактирование категории')
+@section('title', 'Редактирование открытая информация')
 
 @section('content')
     <!-- Content Header (Page header) -->
@@ -8,13 +8,13 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Редактирование категории: {{ $openData->title }}</h1>
+                    <h1 class="m-0">Редактирование открытую информацию: {{ $opendata->id }}</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ route('homeAdmin') }}">Главная</a></li>
-                        <li class="breadcrumb-item"><a href="{{ route('openData.index') }}">Категории</a></li>
-                        <li class="breadcrumb-item active">Редактирование категорию</li>
+                        <li class="breadcrumb-item"><a href="{{ route('opendatas.index') }}">открытая информация</a></li>
+                        <li class="breadcrumb-item active">Редактирование открытую информацию</li>
                     </ol>
                 </div>
 
@@ -31,21 +31,21 @@
                 <div class="col-lg-12">
                     <div class="card card-primary">
                         <!-- form start -->
-                        <form action="{{ route('openData.update', $openData->id) }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('opendatas.update', $opendata->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            @method('PUT')
+                            @method('PATCH')
                             <div class="card-body">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Nomi O'zbekcha</label>
-                                    <input type="text" value="{{ $openData->title_uz }}" name="title_uz" class="form-control" id="exampleInputEmail1" placeholder="Введите название" required>
+                                    <input type="text" value="{{ $opendata->title_uz }}" name="title_uz" class="form-control" id="exampleInputEmail1" placeholder="Введите название" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Название Русский</label>
-                                    <input type="text" value="{{ $openData->title_ru }}" name="title_ru" class="form-control" id="exampleInputEmail1" placeholder="Введите название" required>
+                                    <input type="text" value="{{ $opendata->title_ru }}" name="title_ru" class="form-control" id="exampleInputEmail1" placeholder="Введите название" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Name English</label>
-                                    <input type="text" value="{{ $openData->title_en }}" name="title_en" class="form-control" id="exampleInputEmail1" placeholder="Введите название" required>
+                                    <input type="text" value="{{ $opendata->title_en }}" name="title_en" class="form-control" id="exampleInputEmail1" placeholder="Введите название" required>
                                 </div>
                             </div>
                             <div class="form-group">
