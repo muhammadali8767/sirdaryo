42538cefdb785886dd863bc7013c9be43943373b
Muhammadali <muhammadali8767@gmail.com>
2022-12-23 09:11:16 +0500

    senat

--- resources/views/front/senators.blade.php
+++ resources/views/front/senators.blade.php
@@ -3,21 +3,21 @@
 <div class="container">
     <nav aria-label="breadcrumb" class="breadcrumb-nav">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Mahalliy kengash</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Senat a'zolari</li>
+            <li class="breadcrumb-item"><a href="#">@lang('front.Mahalliy kengash')</a></li>
+            <li class="breadcrumb-item active" aria-current="page">@lang('front.Doimiy komissiya azolari')</li>
         </ol>
     </nav>
     <h2 class="section-title mb-30">
-        Senat a'zolari
+        @lang('front.Doimiy komissiya azolari')
     </h2>
 
     <table class="info-tabel">
         <thead>
             <tr>
                 <th>№</th>
-                <th>Familiyasi ismi va otasining ismi</th>
-                <th>Ish joyi va lavozimi</th>
-                <th>Partiyasi</th>
+                <th>@lang('front.Familiyasi ismi va otasining ismi')</th>
+                <th>@lang('front.Ish joyi va lavozimi')</th>
+                <th>@lang('front.Partiyasi')</th>
             </tr>
         </thead>
         @foreach ($senators as $key => $senator)
